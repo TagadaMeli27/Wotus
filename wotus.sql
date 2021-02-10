570c6379bf2733db806be4e5d4d2663ee5c0f25b
TagadaMeli27 <melissaleray27@hotmail.fr>
2021-02-10 18:05:43 +0100

    fichier sql avec CREATE DATABASE

--- wotus.sql
+++ wotus.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 10 fév. 2021 à 17:28
--- Version du serveur :  10.4.17-MariaDB
--- Version de PHP : 8.0.0
+-- Généré le :  mer. 10 fév. 2021 à 18:03
+-- Version du serveur :  5.7.17
+-- Version de PHP :  7.1.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,8 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `wotus`
+-- Base de données :  `wotus`
 --
+CREATE DATABASE IF NOT EXISTS `wotus` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `wotus`;
 
 -- --------------------------------------------------------
 
@@ -465,8 +468,7 @@ ALTER TABLE `mots`
 -- AUTO_INCREMENT pour la table `mots`
 --
 ALTER TABLE `mots`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=410;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=410;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
